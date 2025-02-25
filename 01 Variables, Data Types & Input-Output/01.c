a60472d874fa9c51fe9897542a625abdb837c306
Smaran Yanapu <smaranyanapu@gmail.com>
2025-02-25 21:40:04 +0530

    create

---
+++ 01 Variables, Data Types & Input-Output/01.c
@@ -0,0 +1,25 @@
+// Program to print different star patterns
+#include<stdio.h>
+
+int main() {
+  printf("Right Half Pyramid\n");
+  printf("*\n");
+  printf("* *\n");
+  printf("* * *\n");
+  printf("* * * *\n");
+  printf("* * * * *\n");
+
+  printf("Reverse Right Half Pyramid\n");
+  printf("* * * * *\n");
+  printf("* * * *\n");
+  printf("* * *\n");
+  printf("* *\n");
+  printf("*\n");
+
+  printf("Left Half Pyramid\n");
+  printf("        *\n");
+  printf("      * *\n");
+  printf("    * * *\n");
+  printf("  * * * *\n");
+  printf("* * * * *\n");
+}
