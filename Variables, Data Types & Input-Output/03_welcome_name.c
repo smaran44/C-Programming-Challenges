aa32dff11b49b93a0e27266cd167a8895aca3a40
Smaran Yanapu <smaranyanapu@gmail.com>
2025-02-26 17:24:44 +0530

    DEL

--- Variables, Data Types & Input-Output/03_welcome_name.c
+++
@@ -1,9 +0,0 @@
-//create a program to input name of the person and respond with "Welcome Name to coding"
-#include <stdio.h>
-
-int main() {
-  char name[20];
-  printf("Please enter your name: ");
-  scanf(" %19s", name);
-  printf("Welcome %s to Coding.", name);
-}
